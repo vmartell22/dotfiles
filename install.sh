f1b1fdec64db97d5f7a5e5d21c3b06a9ed15905f
Victor Martell <vmartell22@gmail.com>
2016-10-10 10:36:15 -0700

    better install for ncmcpp and added spacemacs

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+lsb_release="$(perl -nle 'print $1 if/.*DISTRIB_RELEASE=(.*)/' /etc/lsb-release)"
+
 cp tmux ~/.tmux.conf
 cp screenrc ~/.screenrc
 cp -R vim ~/.vim
 cp vimrc ~/.vimrc
-mkdir ~/.ncmpcpp
-cp ncmpcpp_config ~/.ncmpcpp/config
+cp spacemacs ~/.spacemacs
+
+mkdir -p ~/.ncmpcpp
+
+if [[ "$lsb_release" =~ ^16 ]] || [[ "$lsb_release" =~ ^18 ]]; then
+
+  cp ncmpcpp_config_16.04 ~/.ncmpcpp/config
+
+fi
+
+if [[ "$lsb_release" =~ ^14 ]] || [[ "$lsb_release" =~ ^17 ]] ; then
+
+  cp ncmpcpp_config_14.04 ~/.ncmpcpp/config
+
+fi
+
 
